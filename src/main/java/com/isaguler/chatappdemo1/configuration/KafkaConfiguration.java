4c38fd2d409dd7bde73e49e42f54fc7a4cf2efa6
Gülers <jesus3419@gmail.com>
2022-10-05 16:39:05 +0300

    edit

--- src/main/java/com/isaguler/chatappdemo1/configuration/KafkaConfiguration.java
+++ src/main/java/com/isaguler/chatappdemo1/configuration/KafkaConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
